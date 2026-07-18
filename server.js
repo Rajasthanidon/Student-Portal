1d547c20a7c7c9ea44a22142b05b57e71e6221d4
Rajasthanidon <nirdoshmeena350@gmail.com>
2026-07-18 17:47:00 +0530

    final2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 async function start(){
     await createtables();
     app.listen(PORT,"0.0.0.0",()=>{
-        console.log("server started on ${PORT}")
+        console.log(`server started on ${PORT}`)
     });
 }
 start();
