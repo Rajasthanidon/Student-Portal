6347a743011f04067037ff3fbc3516b3f63fc792
Rajasthanidon <nirdoshmeena350@gmail.com>
2026-07-18 17:28:28 +0530

    final1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const createtables = require("./database/db")
 const PORT = process.env.PORT || 3000;
 async function start(){
     await createtables();
-    app.listen(PORT,()=>{
+    app.listen(PORT,"0.0.0.0",()=>{
         console.log("server started")
     });
 }
