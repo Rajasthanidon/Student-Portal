c0eb9bc8eba9675b23415895682a53316d275ec9
Rajasthanidon <nirdoshmeena350@gmail.com>
2026-07-20 01:51:04 +0530

    import feature added

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 const createtables = require("./database/db")
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 async function start(){
     await createtables();
-    app.listen(PORT,"0.0.0.0",()=>{
+    app.listen(PORT,()=>{
         console.log(`server started on ${PORT}`)
     });
 }
